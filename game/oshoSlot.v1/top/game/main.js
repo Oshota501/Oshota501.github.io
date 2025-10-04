5c10b81dfb37b2bb14409a8a1afe6c2e43b6f0f1
Oshota501 <kakidesu1111@gmail.com>
2025-10-05 01:13:58 +0900

    scoreページ作成

--- game/oshoSlot.v1/top/game/main.js
+++ game/oshoSlot.v1/top/game/main.js
@@ -8,6 +8,7 @@ const slot = new Slot(840,900,[
         PATH_TO_IMAGE : "../img/bell.png", // String
         obj_id : 0 , // Int
         tag : [] ,
+        win_p : "5倍" ,
         score : function(bet,size){
             return 5 * bet  * (size-2)
         } // Function
@@ -17,24 +18,27 @@ const slot = new Slot(840,900,[
         PATH_TO_IMAGE : "../img/apple.png", // String
         obj_id : 1 , // Int
         tag : [] ,
+        win_p : "3倍" ,
         score : function(bet,size){
             return 3 * bet  * (size-2)
         } // Function
     },{
         name : "lemon" ,
-        probability : 20, // Float
+        probability : 24, // Float
         PATH_TO_IMAGE : "../img/lemon.png", // String
         obj_id : 2 , // Int
         tag : [] ,
+        win_p : "2倍" ,
         score : function(bet,size){
             return 2 * bet  * (size-2)
         } // Function
     },{
-        name : "ruby" ,
+        name : "diamond" ,
         probability : 8, // Float
         PATH_TO_IMAGE : "../img/ruby.png", // String
         obj_id : 3 , // Int
         tag : [] ,
+        win_p : "10倍" ,
         score : function(bet,size){
             return 10 * bet  * (size-2)
         } // Function
@@ -44,6 +48,7 @@ const slot = new Slot(840,900,[
         PATH_TO_IMAGE : "../img/seven.png", // String
         obj_id : 4 , // Int
         tag : [] ,
+        win_p : "100倍" ,
         score : function(bet,size){
             return 100 * bet  * (size-2)
         } // Function
@@ -53,8 +58,9 @@ const slot = new Slot(840,900,[
         PATH_TO_IMAGE : "../img/cpp.png", // String
         obj_id : 5 , // Int
         tag : ["programing"] ,
+        win_p : "40倍<br>他の言語と一致して1円" ,
         score : function(bet,size){
-            return 100 * bet  * (size-2)
+            return 40 * bet  * (size-2)
         } // Function
     },{
         name : "java" ,
@@ -62,8 +68,9 @@ const slot = new Slot(840,900,[
         PATH_TO_IMAGE : "../img/java.png", // String
         obj_id : 6 , // Int
         tag : ["programing"] ,
+        win_p : "40倍<br>他の言語と一致して1円" ,
         score : function(bet,size){
-            return 100 * bet  * (size-2)
+            return 40 * bet  * (size-2)
         } // Function
     },{
         name : "py" ,
@@ -71,8 +78,9 @@ const slot = new Slot(840,900,[
         PATH_TO_IMAGE : "../img/py.png", // String
         obj_id : 7 , // Int
         tag : ["programing"] ,
+        win_p : "40倍<br>他の言語と一致して1円" ,
         score : function(bet,size){
-            return 100 * bet  * (size-2)
+            return 40 * bet  * (size-2)
         } // Function
     }
 ]) ;
