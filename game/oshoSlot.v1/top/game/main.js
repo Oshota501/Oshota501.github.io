ea5748936ea641ff8f4c2dfc0e0f1a6a5ef7edd4
Oshota501 <kakidesu1111@gmail.com>
2025-10-05 02:27:22 +0900

    ショップのコンポーネント（？）を作成

--- game/oshoSlot.v1/top/game/main.js
+++ game/oshoSlot.v1/top/game/main.js
@@ -93,3 +93,68 @@ bank.coin = 0
 
 // shopの内容物一覧と出現確率
 
+shop.setItems([
+    {
+        price : 3 ,
+        name : "nomal apple" ,
+        lvl : 1 ,
+        weight : 50 ,
+        explain : "1ターンの間 リンゴが出る確率が少し上がる。",
+        textureURL : "../img/10.png" ,
+        opt : [{
+            add : 3 ,
+            name : "apple" ,
+            fixed : false ,
+            turn : 1 ,
+        }] 
+    },{
+        price : 2 ,
+        name : "nomal lemon" ,
+        lvl : 1 ,
+        weight : 50 ,
+        explain : "1ターンの間 レモンが出る確率が少し上がる。",
+        textureURL : "../img/10.png" ,
+        opt : [{
+            add : 3 ,
+            name : "lemon" ,
+            fixed : false ,
+            turn : 1 ,
+        }]
+    },{
+        price : 7 ,
+        name : "煌めくレモン" ,
+        lvl : 2 ,
+        weight : 20 ,
+        explain : "5ターンの間、レモンが出る確率が少し上がる。",
+        textureURL : "../img/10.png" ,
+        opt : [{
+            add : 2 ,
+            name : "lemon" ,
+            fixed : false ,
+            turn : 5 ,
+        },{
+            add : -4 ,
+            name : "apple" ,
+            fixed : false ,
+            turn : 1 ,
+        },]
+    },{
+        price : 5 ,
+        name : "除草剤" ,
+        lvl : 2 ,
+        weight : 20 ,
+        explain : "5ターンの間 リンゴとレモンの出る確率が下がる。",
+        textureURL : "../img/10.png" ,
+        opt : [{
+            add : -4 ,
+            name : "lemon" ,
+            fixed : false ,
+            turn : 1 ,
+        },{
+            add : -4 ,
+            name : "apple" ,
+            fixed : false ,
+            turn : 1 ,
+        },]
+    },]
+)
